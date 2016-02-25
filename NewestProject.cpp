4e2342ea767c0f8c33bfdd63d812ff29ffb0c1c6
Jin-A <belumah@googlemail.com>
2016-02-25 22:30:32 +0100

    Added files via upload
    Learned about:
    
    Hello world 
    reading and writing data to and from the screen

---
+++ NewestProject.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+ using namespace std;
+
+int main ()
+{
+	double weight = 0;
+	string name;
+
+	cout << "Enter your weight: \n";
+	cin >> weight;
+
+	cout << "Enter your name: \n";
+	cin >> name;
+
+	cout << "Your weight is: " << weight << ".\nYour name is: " << name << ".";
+
+}
